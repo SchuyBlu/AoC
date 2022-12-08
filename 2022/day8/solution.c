1b3692c02df474462bb469338de4c96de80657e9
Schuyler Kelly <schuylertk@gmail.com>
2022-12-08 11:55:33 -0700

    solution.c successfully reads interior

--- 2022/day8/solution.c
+++ 2022/day8/solution.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
 
@@ -35,7 +36,7 @@ char** build_plot(FILE *stream, char **plot, size_t *nrowsl, size_t *nrowsc, siz
             // Stripping the null terminator and newline character, as
             // I don't want a string, just a 2D char array with characters
             // from 0 to 9
-            *row_len = nread - 2;
+            *row_len = nread - 1;
             plot = malloc(sizeof(char*) * *nrowsc);
             exit_on_failure(plot, "Failed to allocate for rows");
 
@@ -62,6 +63,96 @@ char** build_plot(FILE *stream, char **plot, size_t *nrowsl, size_t *nrowsc, siz
 }
 
 
+uint32_t analyze_plot(char **plot, size_t nrowsl, size_t row_len)
+{
+    bool visible_in_column, visible_in_row;
+    bool visible_on_left, visible_on_right;
+    bool visible_on_top, visible_on_bottom;
+    uint32_t count = 0;
+
+    // Go through each character
+    for (int i = 1; i < nrowsl - 1; i++) {
+        for (int j = 1; j < row_len - 1; j++) {
+            // Reset boolean values
+            visible_on_left = true;
+            visible_on_right = true;
+            visible_on_top = true;
+            visible_on_bottom = true;
+            visible_in_column = true;
+            visible_in_row = true;
+
+            // Character is at plot[i][j]
+            // Now check character against all four directions
+            // Check each column first
+            for (int k = 0; k < row_len; k++) {
+                // If k is bigger than j and still visible
+                // in column, break because its visible from
+                // one side
+                if ((k > j) && visible_on_left) break;
+
+                // If checking against itself, ignore
+                if (j == k) continue;
+
+                // Checking left side. If current num is bigger than
+                // num being analyzed, set left bool to false
+                if ((k < j) && plot[i][k] >= plot[i][j])
+                    visible_on_left = false;
+
+                // Check right side using same logic as above
+                if ((k > j) && plot[i][k] >= plot[i][j])
+                    visible_on_right = false;
+
+                // If neither visible on left or visible on right,
+                // visible in column is not true
+                if ((!visible_on_left) && (!visible_on_right))
+                    visible_in_column = false;
+
+                // If not visible in column, break to prevent unecessary
+                // iteration
+                if (!visible_in_column)
+                    break;
+            }
+
+            // Now check each row
+            for (int l = 0; l < nrowsl; l++) {
+                // If l is bigger than i and still visible on top,
+                // break because its visible on one side.
+                if ((l > i) && visible_on_top) break;
+
+                // If checking against itself, ignore
+                if (i == l) continue;
+
+                // Checking top side. If current num is bigger than
+                // num being analyzed, set the top bool to false
+                if ((l < i) && plot[l][j] >= plot[i][j])
+                    visible_on_top = false;
+
+                // Check bottom side using the same logic as above
+                if ((l > i) && plot[l][j] >= plot[i][j])
+                    visible_on_bottom = false;
+
+                // If neither visible on top or visible on bottom,
+                // visible in row is not true
+                if ((!visible_on_top) && (!visible_on_bottom))
+                    visible_in_row = false;
+
+                // If not visible in row, break to prevent unecessary
+                // iteration
+                if (!visible_in_row)
+                    break;
+            }
+
+            // Now that both rows and columns have been checked, if either is true
+            // we can add one to the count
+            if (visible_in_row || visible_in_column) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
 int main(int argc, char **argv)
 {
     // File declarations
@@ -72,14 +163,19 @@ int main(int argc, char **argv)
     size_t nrowsl = 0, nrowsc = 1;
     size_t row_len;
 
+    // Declaration for plot analyzation
+    uint32_t trees_that_are_visible;
+
     stream = fopen(argv[1], "r");
     exit_on_failure(stream, "Failed to open file");
  
     // Build the plot
     plot = build_plot(stream, plot, &nrowsl, &nrowsc, &row_len);
 
-    // Print plot for testing
-    print_plot(plot, nrowsl, row_len);
+    // Analyze the plot
+    trees_that_are_visible = analyze_plot(plot, nrowsl, row_len);
+    printf("%d\n", trees_that_are_visible);
+
     return 0;
 }
 
