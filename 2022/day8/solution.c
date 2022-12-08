fedb6485637cd50cc921ee00c876a97ade1b5eeb
Schuyler Kelly <schuylertk@gmail.com>
2022-12-08 10:00:18 -0700

    Added function to read into the plot

--- 2022/day8/solution.c
+++ 2022/day8/solution.c
@@ -13,16 +13,6 @@ void exit_on_failure(void *ptr, char *message)
 }
 
 
-void resize(void *ptr, size_t *cap, size_t *len, size_t size)
-{
-    if (*cap <= *len) {
-        (*cap) *= 2;
-        ptr = realloc(ptr, size);
-        exit_on_failure(ptr, "Failed to reallocate memory");
-    }
-}
-
-
 void print_plot(char **plot, size_t nrowsl, size_t row_len)
 {
     for (int i = 0; i < nrowsl; i++) {
@@ -33,13 +23,49 @@ void print_plot(char **plot, size_t nrowsl, size_t row_len)
 }
 
 
+char** build_plot(FILE *stream, char **plot, size_t *nrowsl, size_t *nrowsc, size_t *row_len)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread = 0;
+
+    while ((nread = getline(&line, &len, stream)) != -1) {
+        if (plot == NULL) {
+            // Allocate for row position
+            // Stripping the null terminator and newline character, as
+            // I don't want a string, just a 2D char array with characters
+            // from 0 to 9
+            *row_len = nread - 2;
+            plot = malloc(sizeof(char*) * *nrowsc);
+            exit_on_failure(plot, "Failed to allocate for rows");
+
+            // Allocate for row itself
+            plot[*nrowsl] = malloc(sizeof(char) * *row_len);
+            exit_on_failure(plot[*nrowsl], "Failed to allocate for a row"); 
+        }
+        else {
+            // Grow plot if necessary
+            if (*nrowsl >= *nrowsc) {
+                *nrowsc *= 2;
+                plot = realloc(plot, sizeof(char*) * *nrowsc);
+                exit_on_failure(plot, "Failed to reallocate for rows");
+            }
+            // Allocate for row itself
+            plot[*nrowsl] = malloc(sizeof(char) * *row_len);
+            exit_on_failure(plot[*nrowsl], "Failed reto allocate for a row");
+        }
+        // Fill row
+        memcpy(plot[*nrowsl], line, *row_len); 
+        (*nrowsl)++;
+    }
+    return plot;
+}
+
+
 int main(int argc, char **argv)
 {
     // File declarations
     FILE *stream = NULL;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread = 0;
 
     // 2D array declarations
     char **plot = NULL;
@@ -49,35 +75,8 @@ int main(int argc, char **argv)
     stream = fopen(argv[1], "r");
     exit_on_failure(stream, "Failed to open file");
  
-    while ((nread = getline(&line, &len, stream)) != -1) {
-        if (plot == NULL) {
-            // Allocate for row position
-            // Stripping the null terminator and newline character, as
-            // I don't want a string, just a 2D char array with characters
-            // from 0 to 9
-            row_len = nread - 2;
-            plot = malloc(sizeof(char*) * nrowsc);
-            exit_on_failure(plot, "Failed to allocate for rows");
-
-            // Allocate for row itself
-            plot[nrowsl] = malloc(sizeof(char) * row_len);
-            exit_on_failure(plot[nrowsl], "Failed to allocate for a row"); 
-        }
-        else {
-            // Grow plot if necessary
-            if (nrowsl >= nrowsc) {
-                nrowsc *= 2;
-                plot = realloc(plot, sizeof(char*) * nrowsc);
-                exit_on_failure(plot, "Failed to reallocate for rows");
-            }
-            // Allocate for row itself
-            plot[nrowsl] = malloc(sizeof(char) * row_len);
-            exit_on_failure(plot[nrowsl], "Failed reto allocate for a row");
-        }
-        // Fill row
-        memcpy(plot[nrowsl], line, row_len); 
-        nrowsl++;
-    }
+    // Build the plot
+    plot = build_plot(stream, plot, &nrowsl, &nrowsc, &row_len);
 
     // Print plot for testing
     print_plot(plot, nrowsl, row_len);
